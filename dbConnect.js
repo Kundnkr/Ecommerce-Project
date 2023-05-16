6fc90d8009d391cc1b8bc994409e4b27412fd160
kundan Kumar <kkundan501@gmail.com>
2023-05-16 22:21:52 +0530

    first

--- dbConnect.js
+++ dbConnect.js
@@ -5,6 +5,7 @@ let url = "mongodb+srv://kundan:kundan501@cluster0.kl8xdw3.mongodb.net/Finalyear
 
 let dbConnect = async()=>{
    await mongoose.connect(url);
+   console.log("database connected");
 }
 
 module.exports = dbConnect;
